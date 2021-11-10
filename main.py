36c0f75b4a6a57d86e5052c4d31d3649e28e8633
flat35hd99 <38929316+flat35hd99@users.noreply.github.com>
2021-11-10 11:36:43 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from biovoronoi.core import Core
+from Bio.PDB import PDBParser
+
+def main():
+    structure = PDBParser(QUIET=True).get_structure("nve400", "./nve400_wraped.pdb")
+    core = Core()
+    core.set_structure(structure)
+    core.calculate_voronoi_obejct()
+    core.calculate_voronoi_volumes()
+    core.create_df()
+    core.groupby_residue()
+    df = core.get_residue_df()
+    df.to_csv("output.csv")
+    
+if __name__ == "__main__":
+    main()
